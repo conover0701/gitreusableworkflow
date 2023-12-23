e6c37d4e7e920fb7608ac22f8dca23a2d0738b2c
conover0701 <yuanadv@gmail.com>
2023-12-23 12:00:14 +0000

    Fix var name

--- IAMRoles/variables.tf
+++ IAMRoles/variables.tf
@@ -2,3 +2,8 @@ variable "role_name" {
   type        = string
   description = "Specifies the name of the resource group that will be created."
 }
+
+variable "policy_name" {
+  type        = string
+  description = "Specifies the name of the policy that will be created."
+}
